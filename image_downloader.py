d8120c280b69a2b96afbd20dd579098e34862802
w.malik <w.malik@pancakes.live>
2025-02-07 12:06:03 -0500

    progress of active threads is now accessible
    approximation of the number of threads in the queue is now accessible
    
    number of max workers is now accessible
    
    added a simple log system for threads
    
    fixed the ImageDownloader.loop(self) method
    
    fixed the ImageDownloader.stop(self) method
    
    now displays the current progress in the terminal

--- image_downloader.py
+++ image_downloader.py
@@ -17,7 +17,8 @@ class ImageDownloader:
     __n_tasks_lock: threading.Lock
     
     # Information meant for the user of the class
-    active_threads_progress: MutableSequence[float] # % progress TODO: thread safe?
+    n_threads: int
+    active_threads_progress: MutableSequence[float] # % progress
     imgs_metadata: MutableSequence[ImageMetaData] # history of downloaded images
     
     # Attributes set by caller
@@ -31,10 +32,10 @@ class ImageDownloader:
     
     def __init__(self, url, http_headers, use_metadata: bool=True, img_path_format_str: str='%t', directory: str='./imgs/', n_threads: int=None, request_timeout_seconds=5, block_size=4096):
         self.__threadpool = ThreadPoolExecutor(n_threads)
+        self.n_threads = self.__threadpool._max_workers # if n_threads was None, this will find the actual number of threads
         self.__n_tasks_lock = threading.Lock()
         self.__n_tasks = 0
-        
-        self.active_threads_progress = []
+        self.active_threads_progress = [0.0] * self.n_threads
         
         if use_metadata:
             self.imgs_metadata = []
@@ -52,6 +53,9 @@ class ImageDownloader:
         self.__chdir(directory)
         
     
+    @property
+    def qsize(self):
+        return self.__threadpool._work_queue.qsize()
     """Change the directory to which images will be downloaded to path"""
     def __chdir(self, path: str):
         from os import makedirs, chdir
@@ -63,13 +67,13 @@ class ImageDownloader:
     """Keeps calling the looping_function until all tasks are completed"""
     def loop(self, looping_function: Callable) -> None:
         while self.__n_tasks != 0:
-            looping_function()
+            looping_function(self)
     """"Blocks until all tasks in the queue are completed"""
     def wait(self) -> None:
-        self.loop(lambda: None)
+        self.loop(lambda _: None)
     """Stop downloading"""
     def stop(self) -> None:
-        self.__threadpool.shutdown(wait=False)
+        self.__threadpool.shutdown(wait=False, cancel_futures=True)
     
     """Add a task to the threadpool"""
     def __queue_task(self, callback: Callable, task, **task_args) -> None:
@@ -130,19 +134,24 @@ class ImageDownloader:
         self.__queue_task(lambda _: None, self.__blocks_download_img, **parameters)
     """Threaded function for downloading images and saving them to disk"""
     def __blocks_download_img(self, img_src: str, img_path: str) -> None:
-        thread_id = int(threading.current_thread().name[-1])
+        from math import ceil
+        thread_id = int(re.findall(r'_(\d+)$', threading.current_thread().name)[0])
         try:
-            # self.active_threads_progress[thread_id] = 0.0
-            self.active_threads_progress.insert(thread_id, 0.0) # TODO: thread safe?
+            self.active_threads_progress[thread_id] = 0.0 # TODO Thread safe?
             
             img_response = requests.get(img_src, headers=self.http_headers, timeout=self.request_timeout, stream=True)
             img_response.raise_for_status()
             
+            content_length = int(img_response.headers['content-length'])
+            n_blocks = ceil(content_length / self.block_size)
+
             format_extension = re.findall(r'.\w+$', img_src)[-1]
             img_path = img_path + format_extension
             with open(img_path, 'wb') as f:
                 for block in img_response.iter_content(chunk_size=self.block_size):
                     f.write(block)
+                    progress_increment = 100 / n_blocks
+                    self.active_threads_progress[thread_id] += progress_increment
                 f.close()
         except Exception as e:
             ImageDownloader.__handle_thread_exception(thread_id, e)
@@ -189,6 +198,9 @@ class ImageDownloader:
 
     """Static function for handling exceptions that occur inside threads""" 
     def __handle_thread_exception(thread_id: int, e: Exception):
-        print(f'---\nException in thread {thread_id}:\n\t{e}\n---\n')
+        message = f'---\nException in thread {thread_id}:\n\t{e}\n---\n'
+        with open(f'../log_thread{thread_id}.txt', 'a+') as f:
+            f.write(message)
+            f.close()
             
 
--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 from image_downloader import ImageDownloader
 import time
 
-URL = 'https://windows10spotlight.com/page/' # + page number
-N_THREADS = 10
-# dummy User-Agent to avoid 403
-HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'}
+URL = 'https://windows10spotlight.com/page/'
+HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'} # dummy User-Agent to avoid 403
+
+def display_progress(imgd: ImageDownloader):
+    from sys import stdout
+
+    threads_per_row = 4
+    # move cursor up n_threads lines
+    stdout.write(f'\033[{int(imgd.n_threads / threads_per_row)+1}A')
+
+    
+    # loop over every active thread
+    threads_drawn = 0
+    for progress in imgd.active_threads_progress:
+        progress = round(progress)
+
+        # deletes current line and moves cursor to beginning
+        print(f'{progress}  \t', end='')
+        threads_drawn += 1
+        if (threads_drawn % threads_per_row) == 0:
+            print('')
+            print("\033[2K\r", end='')
+
+    print('Tasks in queue: ' + str(imgd.qsize))
+
+        
 
 def main():
-    imgd = ImageDownloader(URL, HEADERS, use_metadata=False, img_path_format_str=r'%d_%t')
+    imgd = ImageDownloader(URL, HEADERS, n_threads=16, use_metadata=True)
     
-    n_pages = 10
+    n_pages = 5
     for i in range(1, n_pages+1):
         imgd.download_page(i)
     
-    imgd.wait()
+
+    print('\033[?25l', end="") # hide cursor
+    print('\n' * 4)
+    try:
+        imgd.loop(display_progress)
+    except KeyboardInterrupt:
+        print('\n' * imgd.n_threads)
+        print('^C detected... cleaning up!')
+        imgd.stop()
+    
+    print('\033[?25h', end="") # show cursor
 
 
 if __name__ == '__main__':
